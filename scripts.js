ca1b992d2af4cd072b4c29b6db296554b72cde0d
TenSketch <bala@tensketch.com>
2021-07-26 13:20:34 +0530

    navbar glassmorphism, intro section update, fonts, scroll to top

---
+++ scripts.js
@@ -0,0 +1,38 @@
+// Initialize Swiper 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+        enabled: true,
+    },
+    breakpoints: {
+        769: {
+            slidesPerView: 4,
+            slidesPerGroup: 3,
+        },
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+//Get the button to top
+var mybutton = document.getElementById("topBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
